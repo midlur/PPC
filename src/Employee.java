cf3005ccdaf87dc0aeb55b1040f97d46fd253789
midlur <midlur999>
2019-07-18 19:30:10 +0530

    first commit

---
+++ src/Employee.java
@@ -0,0 +1,33 @@
+public class Employee {
+    String name;
+    int salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return  name + " " + salary;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            File file = new File(System.getProperty("user.home") + "/Desktop/jj/employee.csv");
+            ArrayList<Employee> employees = new ArrayList();
+
+            Scanner scan = new Scanner(file);
+            scan.useDelimiter(",");
+            int i=0;
+            while(scan.hasNext()){
+                String name = scan.next();
+                int salary = Integer.parseInt(scan.next());
+                employees.add(new Employee(name,salary));
+                i++;
+            }
+
+            employees.sort((Employee s1, Employee s2)->s1.getSalary()-s2.getSalary());
+            Collections.reverse(employees);
+
+            employees.forEach((s)->System.out.println(s));
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
