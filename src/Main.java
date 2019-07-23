1b1b3883eccfe74958d1895a02bd42242f80f4cc
midlur <midlur999>
2019-07-23 15:14:39 +0530

    Corrected

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,7 @@ public class Main {
             Collections.reverse(employees);
 
             employees.forEach((s)->System.out.println(s));
-            System.out.println("Employee with lowest salary is " + employees.get(0));
+            System.out.println("Employee with lowest salary is " + employees.get(10));
         }
         catch(IOException e){
             e.printStackTrace();
